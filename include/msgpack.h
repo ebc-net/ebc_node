70037cc35468af3c36ee60ba74680acaaac5006d
xuxiaona618 <423667720@qq.com>
2019-06-06 16:57:35 +0800

    test pack

--- include/msgpack.h
+++ include/msgpack.h
@@ -13,7 +13,7 @@ class msgPack
 {
 public:
     msgPack(const NET::NodeId& _id);
-    int pack(config::MsgType type, const void *msg=nullptr, void* buf=nullptr, int size=0, config::MsgSubType subType = config::MsgSubType::EMPTY_SUB, int msgSize = 0/*,const NET::NodeId dstId = {nullptr}*/);
+    int pack(config::MsgType type,const void *msg=nullptr, void* buf=nullptr,  int size=0,int aport = 0, config::MsgSubType subType = config::MsgSubType::EMPTY_SUB,int msgSize = 0,NodeId dstId ={0});
     int unpack(const void *buf, int len);
     void msgPrint();
     config::EbcMsg ebcMsg;
